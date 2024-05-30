9b2558be9fa9461970db798c271fb0f6d7bbace9
“ssjosyula” <sandeepsarma.josyula@gmail.com>
2024-05-30 11:52:16 -0400

    Simple form update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './index.css'
 
 function App() {
-  return (
-    <div>
+  const [selectedImage, setSelectedImage] = React.useState(null);
 
-    
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-4">My React App</h1>
-      <p className="text-gray-700">This content is styled with Tailwind CSS!</p>
+  const handleImageChange = (event) => {
+    setSelectedImage(event.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    // Implement your form submission logic here
+    // You can access the selected image using selectedImage state
+
+    console.log("Form Submitted!"); // Example log message
+    console.log(selectedImage)
+    event.preventDefault(); // To prevent the default browser behavior of reloading the page when a form is submitted
+  };
+
+  const handleFileInputClick = () => {
+    document.getElementById('formFile').click();
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center min-vh-100">
+      <Form style={{ border: '1px solid #ccc', padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px' }} onSubmit={handleSubmit}>
+        <h3>Composition breakdown</h3>
+        <Form.Group controlId="formFile" className="mb-3" style={{ display: 'flex', alignItems: 'center', border: '1px solid #ddd', borderRadius: '5px', padding: '10px' }}>
+          <Form.Label htmlFor="formFile" style={{ marginRight: '10px' }}>Upload Image:</Form.Label>
+          <Form.Control type="file" id="formFile" onChange={handleImageChange} style={{ display: 'none' }} />
+          <Button variant="primary" style={{ backgroundColor: '#8BC34A', color: 'white', padding: '5px 10px', borderRadius: '5px', marginRight: '10px' }} onClick={handleFileInputClick}>
+            Choose File
+          </Button>
+          <span style={{ textAlign: 'center' }}>{selectedImage ? selectedImage.name : 'No file selected'}</span>
+        </Form.Group>
+        <Button variant="primary" type="submit" style={{ backgroundColor: '#8BC34A', color: 'white', padding: '10px 20px', borderRadius: '5px', marginTop: '10px' }}>
+          Submit
+        </Button>
+      </Form>
     </div>
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <div>Item 1</div>
-      <div>Item 2</div>
-      <div>Item 3</div>
-    </div>
-      </div>
-  );
+   );
 }
 
 export default App;
